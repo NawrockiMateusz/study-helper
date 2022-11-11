6073f65f99d1230300bdd31e1344a7f57a096158
Mateusz Nawrocki <mateusz.nawroo@gmail.com>
2022-11-11 17:33:03 +0100

    Added datapicker and added style for list task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatNativeDateModule } from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { PageNotExistComponent } from './page-not-exist/page-not-exist.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, TodolistComponent, HomepageComponent, PageNotExistComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TodolistComponent,
+    HomepageComponent,
+    PageNotExistComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,6 +37,11 @@ import { PageNotExistComponent } from './page-not-exist/page-not-exist.component
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
