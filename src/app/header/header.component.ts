29b8507cfc3c280ae22087efef07774c2d20cc1b
NawrockiMateusz <mateusz.nawroo@gmail.com>
2023-06-27 20:53:39 +0200

    Created header of application.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+})
+export class HeaderComponent {}
